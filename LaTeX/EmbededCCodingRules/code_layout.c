886d7d06f4180328add7683aa4dd779e0e76c1c2
Vincent <vincent@project-tomorrow.fr>
2018-09-14 16:35:08 +0200

    Code layout update
    Code layout is now updated to make it easier to read.
    Central tex file change of name from "EmbededCCodingRules" to "general" for easier understanding of its purpose

---
+++ LaTeX/EmbededCCodingRules/code_layout.c
@@ -0,0 +1,16 @@
+// - Les types de variables et leurs mots clef :
+    char    void    const   enum    typedef
+// - Les mots clef du langage C sont en rouge et gras :
+	if    while    sizeof    break
+// - Les chaines de caracteres et les caracteres : 
+    "hello"    '\n'
+// - Les commentaires :
+    /* ... */ ou //...
+// - Les operateurs :
+    =    *    &    |
+// - Les nombres : 
+    0x41 ou    1
+// - Les directives preprocesseur :
+    #define    #ifdef
+
+
